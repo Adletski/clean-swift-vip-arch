c5fc5a7e52bb30047970cab281601814c1db3d73
Adlet <adlet.zh19966@gmail.com>
2023-04-09 22:38:08 +0600

    Initial Commit

---
+++ clean swift vip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clean swift vip
+//
+//  Created by Adlet Zhantassov on 09.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
